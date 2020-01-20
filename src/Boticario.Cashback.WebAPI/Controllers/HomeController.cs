b961a087804c0ce42db14283d7b3bd0c3c471640
Felipe Staskoviak <felipe.staskoviak@hotmail.com>
2020-01-20 13:23:17 -0300

    Api home

---
+++ src/Boticario.Cashback.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Boticario.Cashback.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : BaseControllerCustom
+    {
+       
+        [AllowAnonymous]
+        [HttpGet]
+        public object Get()
+        {
+            return new { message = "Api online!" };
+        }
+
+    }
+}
